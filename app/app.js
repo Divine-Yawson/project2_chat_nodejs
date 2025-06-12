357487b7701a95ed0d032dc30da99b36ba232fb4
Divine-Yawson <divineyawson@gmail.com>
2025-06-12 01:20:23 +0000

    update

--- app/app.js
+++ app/app.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const http = require('http');
-const path = require('path');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the chat-app directory
+// Serve the index.html file
 app.use(express.static(path.join(__dirname)));
 
-// Optional: add an explicit route for "/"
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 io.on('connection', socket => {
-  console.log('User connected');
-  socket.on('disconnect', () => console.log('User disconnected'));
+    console.log('User connected');
+
+    socket.on('chat message', msg => {
+        console.log('Message received:', msg);
+        io.emit('chat message', msg); // Broadcast to all clients
+    });
+
+    socket.on('disconnect', () => console.log('User disconnected'));
 });
 
 server.listen(3001, () => console.log('Chat app running on port 3001'));
-
